f006a83d1a11e1c8d27acb4a0725ab1907d8b438
Annabelle (Annie) Neal <agn66646@uga.edu>
2021-12-15 15:43:25 -0500

    snake adds link when apple is eaten

--- src/main/java/cs1302/game/SnakeGame.java
+++ src/main/java/cs1302/game/SnakeGame.java
@@ -17,6 +17,7 @@ import javafx.scene.text.Text;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
 import javafx.util.Duration;
+import javafx.scene.image.Image;
 
 public class SnakeGame extends VBox {
 
@@ -31,27 +32,29 @@ public class SnakeGame extends VBox {
     private final Timeline loop = new Timeline();
     private final Duration fpsTarget;
     private final Bounds bounds;
-    SnakeFood apple;
+    //SnakeFood apple;
     Canvas canvas;
     GraphicsContext gc;
     boolean gameOver;
     VBox scoreBox;
     Text scoreText;
     int score = 0;
+    int tailX = 0;
+    int tailY = 0;
     private boolean initialized = false;
     String direction = "right";
+    //Image apple;
 
 
     public SnakeGame() {
         //super(GRID_SIZE, GRID_SIZE, 60);
         super();
         setMinWidth(GRID_SIZE);
-        setMinHeight(GRID_SIZE);
+        setMinHeight(GRID_SIZE + 15);
         this.bounds = new BoundingBox(0, 0, ROWS, COLUMNS);
         this.fpsTarget = Duration.millis(400);
         addEventFilter(KeyEvent.KEY_PRESSED, event -> getDirection(event));
         this.snake = new ArrayList<>();
-        this.apple = new SnakeFood(this);
         scoreText = new Text("Score: " + score);
         canvas = new Canvas(GRID_SIZE, GRID_SIZE);
         gc = canvas.getGraphicsContext2D();
@@ -60,17 +63,20 @@ public class SnakeGame extends VBox {
     } // SnakeGame
 
     protected void init() {
-        snake.add(new Snake(COLUMNS / 2, ROWS / 2));
-        snake.add(new Snake((COLUMNS / 2) - 1, ROWS / 2));
-        snake.add(new Snake((COLUMNS / 2) - 2, ROWS / 2));
+        snake.add(new Snake(10, 10));
+        snake.add(new Snake(9, 10));
+        snake.add(new Snake(8, 10));
         //scoreBox.getChildren().add(scoreText);
         getChildren().addAll(scoreText, canvas);
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, GRID_SIZE, GRID_SIZE);
-                for(Snake link : snake) {
+        for(Snake link : snake) {
             gc.setFill(Color.GREEN);
             gc.fillRect(link.x * BOX_SIZE, link.y * BOX_SIZE, BOX_SIZE, BOX_SIZE);
         }
+        moveApple();
+        gc.setFill(Color.RED);
+        gc.fillOval(appleX * BOX_SIZE, appleY * BOX_SIZE, BOX_SIZE, BOX_SIZE);
 
     }
 
@@ -111,13 +117,28 @@ public class SnakeGame extends VBox {
 
 
     protected void update() {
+        if(gameOver) {
+            gc.setFill(Color.RED);
+            gc.fillText("Game Over", 150, 200);
+            return;
+        }
+        tailX = snake.get(snake.size() - 1).x;
+        tailY = snake.get(snake.size() - 1).y;
         for(int i  = snake.size() - 1; i >= 1; i--) {
             snake.get(i).x = snake.get(i - 1).x;
             snake.get(i).y = snake.get(i - 1).y;
         }
+        if(eatApple() == true) {
+            snake.add(snake.size(), new Snake(tailX, tailY));
+        }
         moveSnake();
+        if(checkWallCollision() || checkSelfCollision()) {
+            gameOver = true;
+        }
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, GRID_SIZE, GRID_SIZE);
+        gc.setFill(Color.RED);
+        gc.fillOval(appleX * BOX_SIZE, appleY * BOX_SIZE, BOX_SIZE, BOX_SIZE);
         for(Snake link : snake) {
             gc.setFill(Color.GREEN);
             gc.fillRect(link.x * BOX_SIZE, link.y * BOX_SIZE, BOX_SIZE, BOX_SIZE);
@@ -166,35 +187,37 @@ public class SnakeGame extends VBox {
     protected void moveSnake() {
         if(direction.equals("left")) {
             snake.get(0).x--;
-            if(checkWallCollision() || checkSelfCollision()) {
-                gameOver = true;
-            }
+            //if(checkWallCollision() || checkSelfCollision()) {
+            //gameOver = true;
+            //}
         } else if(direction.equals("right")) {
             snake.get(0).x++;
-            if(checkWallCollision() || checkSelfCollision()) {
-                gameOver = true;
-            }
+            //if(checkWallCollision() || checkSelfCollision()) {
+                //  gameOver = true;
+            //}
         } else if(direction.equals("up")) {
             snake.get(0).y--;
-            if(checkWallCollision() || checkSelfCollision()) {
-                gameOver = true;
-            }
+            //if(checkWallCollision() || checkSelfCollision()) {
+            //  gameOver = true;
+            //}
         } else if(direction.equals("down")) {
             snake.get(0).y++;
-            if(checkWallCollision() || checkSelfCollision()) {
-                gameOver = true;
-            }
+            //if(checkWallCollision() || checkSelfCollision()) {
+            // gameOver = true;
+            //}
         }
 
     } // moveSnake
 
-    protected void eatApple() {
+    protected boolean eatApple() {
         if(appleX == getHeadX() && appleY == getHeadY()) {
-            // add new link
-            // update score
-            // place apple somewhere else
+            score++;
+            scoreText.setText("Score: " + score);
+            moveApple();
+            return true;
+        } else {
+            return false;
         }
-
     }
 
 
